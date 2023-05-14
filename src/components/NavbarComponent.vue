c0d28476e0e989e88c7882b849e08fd447e6ecfb
jasm <sierramendezjosealejandro@gmail.com>
2023-05-13 23:20:19 -0400

    [Vue, Sass]Refactoring navbar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,35 +1,47 @@
 <template>
-    <div class="container mx-auto">
-        <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-            <div class="flex justify-start">
-                <a
-                    href="#"
-                    class="flex items-center"
-                >
-                    <img
-                        src="../assets/logo.svg"
-                        class="pr-1.5"
-                        alt="Takeit Logo"
-                        width="30"
-                        height="30"
+    <div class="shadow-md">
+        <div class="container mx-auto">
+            <nav class="flex justify-between py-5 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+                <div class="flex">
+                    <a
+                        href="#"
+                        class="flex items-center pr-9"
                     >
-                    <span class="text-logo font-bold">Takeit</span>
-                </a>
-                <div class="hidden w-full md:block md:w-auto">
-                    <ul class="flex flex-row">
-                        <li class="block">
-                            Home
-                        </li>
-                        <li class="block">
-                            About
-                        </li>
-                        <li class="block">
-                            Shop
-                        </li>
-                    </ul>
+                        <img
+                            src="../assets/logo.svg"
+                            class="pr-1.5"
+                            alt="Takeit Logo"
+                            width="30"
+                            height="30"
+                        >
+                        <span class="text-logo font-bold">
+                            {{ $t('navbar.name_logo') }}
+                        </span>
+                    </a>
+                    <div class="block flex items-center">
+                        <ul class="flex flex-row">
+                            <li class="block pr-12 text-primary">
+                                {{ $t('navbar.home') }}
+                            </li>
+                            <li class="block pr-12 text-primary">
+                                {{ $t('navbar.about') }}
+                            </li>
+                            <li class="block pr-12 text-primary">
+                                {{ $t('navbar.shop') }}
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+                <div class="flex items-center">
+                    <input
+                        class="shadow-md input-search rounded-full"
+                        type="text"
+                    >
+                    <span class="text-primary pl-9">shop</span>
+                    <span class="text-primary pl-9">user</span>
+                </div>
+            </nav>
+        </div>
     </div>
 </template>
 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,8 @@
 <template>
     <div>
-        <h1>{{ msg }}</h1>
+        <h1 class="text-center pt-8">
+            {{ msg }}
+        </h1>
     </div>
 </template>
 
@@ -13,20 +15,4 @@ export default {
         }
     }
 };
-
-// import { ref, onMounted, reactive } from 'vue';
-// import axios from 'axios';
-
-// const msg = 'Welcome';
-// const count = ref(0);
-// const responseData = reactive({});
-
-// onMounted(async () => {
-//     try {
-//         const response = await axios.get(import.meta.env.VITE_API_URL + '/getUser');
-//         responseData.value = response.data;
-//     } catch (error) {
-//         console.error(error);
-//     }
-// });
 </script>
