331eebd0121c033af9f12935f28454a664b890a8
jasm <sierramendezjosealejandro@gmail.com>
2023-05-15 17:07:55 -0400

    [Vue, Sass]Refactoring HomePage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,8 +1,27 @@
 <template>
-    <div class="flex items-center justify-center h-screen">
-        <h1 class="text-center pt-8">
-            {{ msg }}
-        </h1>
+    <div class="container mx-auto">
+        <div class="grid grid-cols-2 gap-2">
+            <div class="text-center">
+                <div class="px-3">
+                    <span class="text-title font-size-lg font-bold drop-shadow-md">
+                        Thousands of people they already trust
+                    </span>
+                    <span class="text-cyan-500 font-size-lg font-bold drop-shadow-md"> Takeit</span>
+                </div>
+                <div class="px-3">
+                    <span class="drop-shadow-md text-gray-700">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                    </span>
+                </div>
+                <div class="px-3 flex items-center">
+                    <div>Shop</div>
+                    <div>Shop</div>
+                </div>
+            </div>
+            <div>
+                World
+            </div>
+        </div>
     </div>
 </template>
 
@@ -16,3 +35,8 @@ export default {
     }
 };
 </script>
+
+
+<style lang="sass">
+    @import '../scss/home_page.sass'
+</style>
