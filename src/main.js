b93652d913d7684d96818340f24b3ca6527055e2
jasm <sierramendezjosealejandro@gmail.com>
2023-05-16 15:07:27 -0400

    Integrate pinia

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue'
 import router from './router'
 import i18n from './plugins/i18n'
 import vuetify from './plugins/vuetify'
+import pinia from './plugins/pinia'
 
 const app = createApp(App)
 app.use(router)
 app.use(vuetify)
 app.use(i18n)
+app.use(pinia)
 app.mount('#app')
