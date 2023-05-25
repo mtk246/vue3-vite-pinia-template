6c682066aae7393dd59c943bbe4841dcdb1dad29
minthukyaw <minthukyaw454@gmail.com>
2023-05-25 22:03:57 +0630

    [js, vue, json] handle 404

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="d-flex items-center align-middle flex-col my-10">
+        <div>
+            <h1 class="text-4xl">
+                {{ $t('page_not_found.page_not_found') }}
+            </h1>
+        </div>
+        <div class="my-4">
+            <p>
+                {{ $t('page_not_found.content') }}
+            </p>
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'NotFound',
+};
+</script>
+  
